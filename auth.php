cb21809ed94532aa38aa120da22400d107b79a94
EstherAtiny <98972201+EstherAtiny@users.noreply.github.com>
2022-12-09 22:19:25 -0600

    Update auth.php

--- auth.php
+++ auth.php
@@ -8,9 +8,7 @@ $password = remove_junk($_POST['password']);
 if(empty($errors)){
   $user_id = authenticate($username, $password);
   if($user_id){
-    //create session with id
      $session->login($user_id);
-    //Update Sign in time
      updateLastLogIn($user_id);
      $session->msg("s", "Bienvenido a Tecno BRABE.   -Se implemento JQuery");
      redirect('home.php',false);
