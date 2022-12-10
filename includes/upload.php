5d0e719fbdabc95e0b056e4c60a5a79e917f0ed3
EstherAtiny <98972201+EstherAtiny@users.noreply.github.com>
2022-12-09 23:06:25 -0600

    Update upload.php

--- includes/upload.php
+++ includes/upload.php
@@ -73,9 +73,7 @@ class  Media {
      return true;
     endif;
  }
- /*--------------------------------------------------------------*/
 
- /*--------------------------------------------------------------*/
   public function process_media(){
     if(!empty($this->errors)){
         return false;
@@ -110,9 +108,6 @@ class  Media {
     }
 
   }
-  /*--------------------------------------------------------------*/
-
-  /*--------------------------------------------------------------*/
  public function process_user($id){
 
     if(!empty($this->errors)){
@@ -149,9 +144,6 @@ class  Media {
        }
     }
  }
- /*--------------------------------------------------------------*/
-
- /*--------------------------------------------------------------*/
   private function update_userImg($id){
      global $db;
       $sql = "UPDATE users SET";
@@ -161,9 +153,6 @@ class  Media {
       return ($result && $db->affected_rows() === 1 ? true : false);
 
    }
- /*--------------------------------------------------------------*/
-
- /*--------------------------------------------------------------*/
   public function user_image_destroy($id){
      $image = find_by_id('users',$id);
      if($image['image'] === 'no_image.jpg')
@@ -175,9 +164,6 @@ class  Media {
      }
 
    }
-/*--------------------------------------------------------------*/
-
-/*--------------------------------------------------------------*/
   private function insert_media(){
 
          global $db;
@@ -190,9 +176,6 @@ class  Media {
        return ($db->query($sql) ? true : false);
 
   }
-/*--------------------------------------------------------------*/
-
-/*--------------------------------------------------------------*/
    public function media_destroy($id,$file_name){
      $this->fileName = $file_name;
      if(empty($this->fileName)){
