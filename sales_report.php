8660d17fb2317d72f8c6db240b1b86d0e96e04b0
EstherAtiny <98972201+EstherAtiny@users.noreply.github.com>
2022-12-09 21:16:58 -0600

    Update sales_report.php

--- sales_report.php
+++ sales_report.php
@@ -1,7 +1,6 @@
 <?php
 $page_title = 'Reporte de ventas';
   require_once('includes/load.php');
-  // Checkin What level user has permission to view this page
    page_require_level(3);
 ?>
 <?php include_once('layouts/header.php'); ?>
